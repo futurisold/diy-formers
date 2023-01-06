04b0bec653bf6b3303a68de94588ac7ba7ab2cc0
leoentersthevoid <c.leov@icloud.com>
2023-01-06 17:30:19 +0200

    backbone; tbd

---
+++ transformer.py
@@ -0,0 +1,45 @@
+'''
+Vanilla implementation of the transformer architecture as described in Vaswani et al. 2017: https://arxiv.org/abs/1706.03762
+'''
+
+from beartype import beartype
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+@beartype
+class Embeddings:
+    pass
+
+
+@beartype
+class PositionalEncoding:
+    pass
+
+
+@beartype
+class PositionWiseFeedForward:
+    pass
+
+
+@beartype
+class MultiHeadAttention:
+    pass
+
+
+@beartype
+class EncoderLayer:
+    pass
+
+
+@beartype
+class DecoderLayer:
+    pass
+
+
+@beartype
+class Transformer:
+    pass
+
